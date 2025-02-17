a0e3dff8fb02dd1ae112e69c4d0bcd042b41bbf4
Andi Usman Balo <andibalo213@gmail.com>
2025-02-17 20:11:40 +0700

    Step 10. Work with lists

---
+++ app/src/main/java/id/ac/umn/basicstatecodelab/WellnessTasksList.kt
@@ -0,0 +1,24 @@
+package id.ac.umn.basicstatecodelab
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = remember { getWellnessTasks() }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(list) { task ->
+            WellnessTaskItem(taskName = task.label)
+        }
+    }
+}
+
+fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
