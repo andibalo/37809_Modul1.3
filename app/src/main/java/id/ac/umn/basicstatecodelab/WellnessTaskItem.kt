86d72065ae806e32b0be13553524449859248e55
Andi Usman Balo <andibalo213@gmail.com>
2025-02-17 21:45:14 +0700

    Step 12. State in ViewModel

--- app/src/main/java/id/ac/umn/basicstatecodelab/WellnessTaskItem.kt
+++ app/src/main/java/id/ac/umn/basicstatecodelab/WellnessTaskItem.kt
@@ -12,25 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Checkbox
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-
-@Composable
-fun WellnessTaskItem(
-    taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier
-) {
-    var checkedState by rememberSaveable { mutableStateOf(false) }
-
-    WellnessTaskItem(
-        taskName = taskName,
-        checked = checkedState,
-        onCheckedChange = { newValue -> checkedState = newValue },
-        onClose = onClose,
-        modifier = modifier,
-    )
-}
 
 @Composable
 fun WellnessTaskItem(
